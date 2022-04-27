73a61731d5fdf52e7da405823ed2052a3b8064ac
zuramai <saugi.dev@gmail.com>
2022-04-27 11:05:19 +0700

    feat: basic templating

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,18 @@ module.exports = {
     "./index.html"
   ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      screens: {
+        sm: '600px',
+        md: '728px',
+        lg: '984px',
+        xl: '1240px',
+        '2xl': '1240px',
+      },
+    },
+    extend: {
+    },
   },
   plugins: [],
 }
