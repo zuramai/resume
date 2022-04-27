5e9f546ae93218afc198058eb6a66584af9a0841
zuramai <saugi.dev@gmail.com>
2022-04-27 12:39:51 +0700

    feat: add work experiences

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import axios from 'axios'
+import hbs from 'handlebars';
 import handlebars from 'vite-plugin-handlebars';
+import json from "./resume.json"
 
-const resumeURL = "https://gist.githubusercontent.com/zuramai/5481baa53f9b634362cce9d23c090da5/raw/"
+const resumeURL = "https://gist.githubusercontent.com/zuramai/5481baa53f9b634362cce9d23c090da5/raw"
 
 const getResumeJSON = async () => {
     let resume = {}
@@ -14,16 +16,25 @@ const getResumeJSON = async () => {
 }
 
 export default defineConfig(async ({ command, mode }) => {
-    
     return {
         plugins: [
             handlebars({
                 context: async () => {
-                    const resume = await getResumeJSON()
+                    const resume = json
                     return resume
                 },
+                helpers: {
+                    breaklines: (text) => {
+                        text = hbs.Utils.escapeExpression(text);
+                        text = text.replace(/(\r\n|\n|\r)/gm, '<br>');
+                        return new hbs.SafeString(text);
+                    },
+                    isNotEmpty: (arr) => {
+                        return arr.length > 0
+                    }
+                }
             })
-        ]
+        ],
     }
   })
   
